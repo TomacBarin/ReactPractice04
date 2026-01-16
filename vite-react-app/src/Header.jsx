cfd1c34e927b30def5508a01f01b6f6e19bded79
Tomac Barin Jansson <tomacbarin@me.com>
2026-01-16 12:37:03 +0100

    Tjosan.

---
+++ vite-react-app/src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(props) {
+    return(
+        <header>
+            <h1>Tjosan från {props.name}</h1>
+            <p>Detta är från {props.stad}</p>
+            <p>Gone {props.interest}</p>
+        </header>
+    )
+}
